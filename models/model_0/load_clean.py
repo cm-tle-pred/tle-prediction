7d3054c45aa7f598077a5a706299d351023f27cf
cassova <miller.nicholas.a@gmail.com>
2021-03-26 19:42:10 +0900

    WIP to support list of data_types

--- models/model_0/load_clean.py
+++ models/model_0/load_clean.py
@@ -3,14 +3,14 @@ import numpy as np
 import os
 from tqdm import tqdm
 
-def load_norads(data_type='train'):
+def load_norads(data_types=['train'], debug=False):
     '''
     Loads the NORAD IDs for each specific phase
 
     Parameters
     ----------
-    type : str
-        one of the following: train, validate, test (default is train)
+    type : list
+        list of strings containing the following possibilities: train, validate, test (default is ['train'])
 
         NOTES:
         validate and test are the same data.
@@ -18,51 +18,71 @@ def load_norads(data_type='train'):
 
     Returns
     -------
-    list
-        a list of NORAD IDs
+    dict
+        a dictionary of lists containing the NORAD IDs for each data_type
 
     Raises
     ------
     ValueError
         If unexpected data_type
     '''
-    if data_type == 'train':
-        file_name = 'train_norads.pkl.gz'
-    elif data_type in ('validate', 'test'):
-        file_name = 'validate_norads.pkl.gz'
-    elif data_type == 'secret_test':
-        file_name = 'test_norads.pkl.gz'
-    else:
-        raise ValueError(f'Unexpected data_type when loading NORAD IDs: {data_type}')
 
-    norad_df = pd.read_pickle(os.environ['my_home_path'] + '/data/split_by_norad/' + file_name)
-    norad_list = norad_df.norad.to_list()
-    return norad_list
+    norad_lists = {}
 
-def load_data(norad_list, use_all_data=False):
+    for data_type in data_types:
+        if data_type == 'train':
+            file_name = 'train_norads.pkl.gz'
+        elif data_type in ('validate', 'test'):
+            file_name = 'validate_norads.pkl.gz'
+            data_type = 'test'
+        elif data_type == 'secret_test':
+            file_name = 'test_norads.pkl.gz'
+        else:
+            raise ValueError(f'Unexpected data_type when loading NORAD IDs: {data_type}')
+
+        if debug:
+            print(f'Loading NORAD list from file: {file_name}')
+
+        norad_df = pd.read_pickle(os.environ['my_home_path'] + '/data/split_by_norad/' + file_name)
+        norad_list = norad_df.norad.to_list()
+        norad_lists[data_type] = norad_list
+
+    # Show the results of the norad id load
+    if debug:
+        for k,v in norad_lists.items():
+            print(f' {k} has {len(v)} NORAD IDs')
+
+    return norad_lists
+
+def load_data(norad_lists, use_all_data=False, debug=False):
     '''
     Load gp_history csv.gz files into a pandas dataframe
     '''
-    if use_all_data==0:
+
+    if use_all_data==True:
         csv_store_path = os.environ['GP_HIST_PATH']
     else:
         csv_store_path = os.environ['my_home_path'] + '/data/space-track-gp-hist-sample'
 
+    if debug:
+        print(f'Loading files from path: {csv_store_path}')
+
     necessary_columns = ['NORAD_CAT_ID','OBJECT_TYPE','OBJECT_NAME','TLE_LINE1','TLE_LINE2',
                          'MEAN_MOTION_DOT', 'MEAN_MOTION_DDOT', 'BSTAR', 'INCLINATION', 'RA_OF_ASC_NODE',
                          'ECCENTRICITY', 'ARG_OF_PERICENTER', 'MEAN_ANOMALY', 'MEAN_MOTION', 'EPOCH']
-    dfs = None
+
     files = sorted([x for x in os.listdir(f'{csv_store_path}/') if x.endswith(".csv.gz")])
+    df_list = []
     for f in tqdm(files):
-        df = pd.read_csv(f'{csv_store_path}/{f}', parse_dates=['EPOCH'], infer_datetime_format=True, compression='gzip')
+        df = pd.read_csv(f'{csv_store_path}/{f}',
+                         parse_dates=['EPOCH'],
+                         infer_datetime_format=True,
+                         compression='gzip',
+                         low_memory=False)
         df = df[df.NORAD_CAT_ID.isin(norad_list)][necessary_columns]
+        df_list.append(df)
 
-        # Since animated gabbard diagrams are generated per frame, we can revert the scaling when we plot the graphs
-        if dfs is None:
-            dfs = df
-        elif len(df) > 0:
-            dfs = pd.concat([dfs,df])
-
+    dfs = pd.concat(df_list)
     dfs = dfs.reset_index()
     return dfs
 
@@ -70,18 +90,16 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('data', help='While data to load (train/validate/test) Default=train.  NOTE: validate and test are the same.  To access final test, use secret_test')
-    parser.add_argument('use_all_data', help='Use all gp_history data (1=Yes/0=No) Default=0')
+    parser.add_argument('-t', help='While data to load (train/validate/test). NOTE: validate and test are the same.  To access final test, use secret_test')
+    parser.add_argument('--use_all_data', action='store_const', const=True, default=False, help='Use all gp_history data.')
 
-
-    # TO DO - make the use_all_data not use a 1/0 value.  see here: https://docs.python.org/3/library/argparse.html
-
-    # parser.add_argument(
-    #     'input_file', help='the raw trips without temp file (CSV)')
-    # parser.add_argument(
-    #     'output_file', help='the clean trips without temp file (CSV)')
     args = parser.parse_args()
 
+    # Get the NORAD list
+    #norad_list = load_norads(args.data_type, True)
+    norad_list = load_norads(['train', 'test', 'train', 'validate'], True)
 
-    # clean = clean(args.input_file)
-    # clean.to_csv(args.output_file, index=False)
+    # Get the pandas dataframe
+    #df = load_data(norad_list, args.use_all_data, True)
+
+    #print(df.head())
