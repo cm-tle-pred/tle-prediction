4f835406a3e27983a040499138ef08ecc71f8965
cassova <miller.nicholas.a@gmail.com>
2021-03-29 18:55:48 +0900

    Improved the multi-threading/processing

--- models/model_0/load_data.py
+++ models/model_0/load_data.py
@@ -2,19 +2,26 @@ import pandas as pd
 import numpy as np
 import os
 from tqdm import tqdm
+import concurrent.futures
+from itertools import repeat
 
 def load_norads(data_types=['train'], debug=False):
     '''
-    Loads the NORAD IDs for each specific phase
+    Loads the NORAD IDs for each data_type
 
     Parameters
     ----------
     data_types : list
-        list of strings containing the following possibilities: train, validate, test (default is ['train'])
+        list of strings containing the following possibilities: train, validate,
+        test.  Default is ['train'].
 
         NOTES:
         validate and test are the same data.
         To access the final test data, use 'secret_test'
+        Expected filenames
+         - train: train_norads.pkl.gz
+         - validate / test: validate_norads.pkl.gz
+         - secret_test: test_norads.pkl.gz
 
     debug : bool
         Print debug messages.  Default is False.
@@ -61,19 +68,19 @@ def load_norads(data_types=['train'], debug=False):
     return norad_lists
 
 def __load_task(norad_lists, file_path):
+    #def __load_task(file_path):
     '''
-    Concurrent/Multiprocessing task that loads a csv.gz file
+    Concurrent/Multiprocessing supported task that loads a csv.gz file
     '''
-
-    necessary_columns = ['NORAD_CAT_ID','OBJECT_TYPE','OBJECT_NAME','TLE_LINE1','TLE_LINE2',
-                         'MEAN_MOTION_DOT', 'MEAN_MOTION_DDOT', 'BSTAR', 'INCLINATION', 'RA_OF_ASC_NODE',
-                         'ECCENTRICITY', 'ARG_OF_PERICENTER', 'MEAN_ANOMALY', 'MEAN_MOTION', 'EPOCH']
+    necessary_columns = ['NORAD_CAT_ID','OBJECT_TYPE','OBJECT_NAME','TLE_LINE1',
+                         'TLE_LINE2','MEAN_MOTION_DOT', 'MEAN_MOTION_DDOT',
+                         'BSTAR', 'INCLINATION', 'RA_OF_ASC_NODE',
+                         'ECCENTRICITY', 'ARG_OF_PERICENTER', 'MEAN_ANOMALY',
+                         'MEAN_MOTION', 'EPOCH']
     df_dict = {}
     for data_type in norad_lists.keys():
         df_dict[data_type] = []
     try:
-        # df = pd.read_csv(file_path, compression='gzip', low_memory=False)
-        # df = df[(df.MEAN_MOTION > 11.25) & (df.ECCENTRICITY < 0.25) & (df.OBJECT_TYPE != 'PAYLOAD')]
         df = pd.read_csv(file_path,
                          parse_dates=['EPOCH'],
                          infer_datetime_format=True,
@@ -81,14 +88,28 @@ def __load_task(norad_lists, file_path):
                          low_memory=False)
         for data_type, norad_list in norad_lists.items():
             df_dict[data_type] = df[df.NORAD_CAT_ID.isin(norad_list)][necessary_columns]
-    except:
-        raise Exception(f'Failed to open {file_path}')
+    except Exception as e:
+        raise Exception(f'Failed to open {file_path}.  Error: {e}')
     return df_dict
+    # necessary_columns = ['NORAD_CAT_ID','OBJECT_TYPE','OBJECT_NAME','TLE_LINE1',
+    #                      'TLE_LINE2','MEAN_MOTION_DOT', 'MEAN_MOTION_DDOT',
+    #                      'BSTAR', 'INCLINATION', 'RA_OF_ASC_NODE',
+    #                      'ECCENTRICITY', 'ARG_OF_PERICENTER', 'MEAN_ANOMALY',
+    #                      'MEAN_MOTION', 'EPOCH']
+    # try:
+    #     df = pd.read_csv(file_path,
+    #                      parse_dates=['EPOCH'],
+    #                      infer_datetime_format=True,
+    #                      compression='gzip',
+    #                      low_memory=False)
+    #     return df[necessary_columns]
+    # except Exception as e:
+    #     raise Exception(f'Failed to open {file_path}.  Error: {e}')
 
-def load_data_multi(norad_lists, use_all_data=False, debug=False, threaded=False):
+def load_data(norad_lists, use_all_data=False, debug=False, threaded=False,
+              multiproc=False):
     '''
-    Load gp_history csv.gz files into a pandas dataframe using multiple
-    processes or threads
+    Load gp_history csv.gz files into a pandas dataframe.
 
     Parameters
     ----------
@@ -98,12 +119,17 @@ def load_data_multi(norad_lists, use_all_data=False, debug=False, threaded=False
 
     use_all_data : bool
         Use all the .csv.gz gp_history files.  Default is False.
+        False: %GP_HIST_PATH%
+        True: %my_home_path%/data/space-track-gp-hist-sample
 
     debug : bool
         Print debug messages.  Default is False.
 
     threaded : bool
-        Use threads instead of processes.  Default is False.
+        Use multiple threads.  Default is False.
+
+    multiproc : bool
+        Use multiple processes.  Default is False.
 
     Returns
     -------
@@ -128,102 +154,67 @@ def load_data_multi(norad_lists, use_all_data=False, debug=False, threaded=False
     if debug:
         print(f'Loading files from path: {csv_store_path}')
 
-    files = sorted([x for x in os.listdir(f'{csv_store_path}/') if x.endswith(".csv.gz")])
+    files = sorted([f'{csv_store_path}/{x}' for x in os.listdir(f'{csv_store_path}/') if x.endswith(".csv.gz")])
     df_dict = {}
     for data_type in norad_lists.keys():
         df_dict[data_type] = []
 
     if threaded:
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            results = executor.map(__load_task, files)
-            if debug:
-                print('Finished loading all files. Starting assembly.')
+            results = list(tqdm(executor.map(__load_task, repeat(norad_lists), files), total=len(files)))
             for result in results:
-                for data_type, df in result:
+                for data_type, df in result.items():
                     df_dict[data_type].append(df)
             if debug:
-                print('Finished assembly.')
+                print('Finished loading.')
 
-    else:
+    elif multiproc:
         with concurrent.futures.ProcessPoolExecutor() as executor:
-            results = executor.map(__load_task, files)
-            if debug:
-                print('Finished loading all files. Starting assembly.')
+            results = list(tqdm(executor.map(__load_task, repeat(norad_lists), files), total=len(files)))
             for result in results:
-                for data_type, df in result:
+                for data_type, df in result.items():
                     df_dict[data_type].append(df)
             if debug:
-                print('Finished assembly.')
+                print('Finished loading.')
+        # df = pd.DataFrame()
+        # with tqdm(total=len(files)) as pbar:
+        #     with concurrent.futures.ProcessPoolExecutor() as executor:
+        #         tasks = [executor.submit(__load_task, f) for f in files]
+        #         for t in concurrent.futures.as_completed(tasks):
+        #             df = pd.concat([df, t.result()])
+        #             pbar.update(1)
+        # df_out = {}
+        # for data_type, norad_list in tqdm(norad_lists.items()):
+        #     df_out[data_type] = df[df.NORAD_CAT_ID.isin(norad_list)]
+        # if debug:
+        #     print('Finished loading.')
 
-    df_out = {}
-    for data_type, df_list in df_dict.items():
-        df_out[data_type] = pd.concat(df_list).reset_index()
-    return df_out
-
-def load_data(norad_lists, use_all_data=False, debug=False):
-    '''
-    Load gp_history csv.gz files into a pandas dataframe
-
-    Parameters
-    ----------
-    norad_lists : dict
-        key : data_type (such as train, validate, or test )
-        value : list of ints containing the NORAD IDs within in output dataframe
-
-    use_all_data : bool
-        Use all the .csv.gz gp_history files.  Default is False.
-
-    debug : bool
-        Print debug messages.  Default is False.
-
-    Returns
-    -------
-    dict
-        a dictionary of pandas dataframes containing the TLE gp_history for all
-        NORAD IDs for each data_type
-
-    Raises
-    ------
-    ValueError
-        If norad_lists is empty
-    '''
-
-    if len(norad_lists.keys()) == 0:
-        raise ValueError('norad_lists must contain at least one list')
-
-    if use_all_data==True:
-        csv_store_path = os.environ['GP_HIST_PATH']
     else:
-        csv_store_path = os.environ['my_home_path'] + '/data/space-track-gp-hist-sample'
-
-    if debug:
-        print(f'Loading files from path: {csv_store_path}')
-
-    necessary_columns = ['NORAD_CAT_ID','OBJECT_TYPE','OBJECT_NAME','TLE_LINE1','TLE_LINE2',
-                         'MEAN_MOTION_DOT', 'MEAN_MOTION_DDOT', 'BSTAR', 'INCLINATION', 'RA_OF_ASC_NODE',
-                         'ECCENTRICITY', 'ARG_OF_PERICENTER', 'MEAN_ANOMALY', 'MEAN_MOTION', 'EPOCH']
-
-    files = sorted([x for x in os.listdir(f'{csv_store_path}/') if x.endswith(".csv.gz")])
-    df_dict = {}
-    for data_type in norad_lists.keys():
-        df_dict[data_type] = []
-
-    for f in tqdm(files):
-        df = pd.read_csv(f'{csv_store_path}/{f}',
-                         parse_dates=['EPOCH'],
-                         infer_datetime_format=True,
-                         compression='gzip',
-                         low_memory=False)
-        for data_type, norad_list in norad_lists.items():
-            odf = df[df.NORAD_CAT_ID.isin(norad_list)][necessary_columns]
-            df_dict[data_type].append(odf)
+        for f in tqdm(files):
+            for data_type, df in __load_task(norad_lists, f).items():
+                df_dict[data_type].append(df)
+        print('Finished loading.')
 
     df_out = {}
-    for data_type, df_list in df_dict.items():
-        df_out[data_type] = pd.concat(df_list).reset_index()
+    for data_type, df_list in tqdm(df_dict.items()):
+        df_out[data_type] = pd.concat(df_list).reset_index(drop=True)
+    if debug:
+        print('Finished assembling.')
     return df_out
 
-def write_raw_data(df_dict, use_all_data=False, debug=False, sub_path='/raw_compiled'):
+def __write_task(df, path, debug=False):
+    '''
+    Concurrent/Multiprocessing supported task that saves a df to pickle file
+    '''
+    if debug:
+        print(f'Writing raw data for to: {path}')
+    df.to_pickle(path)
+    if debug:
+        print(f'Finished saving {path}')
+
+def write_data(df_dict, use_all_data=False, debug=False,
+                   sub_path='/raw_compiled', threaded=False,
+                   multiproc=False):
     '''
     Writes all dataframes in df_dict to separate pickle files.
 
@@ -234,13 +225,22 @@ def write_raw_data(df_dict, use_all_data=False, debug=False, sub_path='/raw_comp
         value : list of ints containing the NORAD IDs within in output dataframe
 
     use_all_data : bool
-        Use all the .csv.gz gp_history files.  Default is False.
+        Use the path with all the .csv.gz gp_history files.  Default is False.
+        False: %GP_HIST_PATH% + <sub_path>
+        True: %my_home_path%/data/space-track-gp-hist-sample + <sub_path>
 
     debug : bool
         Print debug messages.  Default is False.
 
     sub_path : str
-        sub path where to write pickle files to.  Default is raw_compiled.
+        sub path where to write pickle files to.  Default is '/raw_compiled'.
+        Must be prefixed with '/'
+
+    threaded : bool
+        Use multiple threads.  Default is False.
+
+    multiproc : bool
+        Use multiple processes.  Default is False.
 
     Raises
     ------
@@ -248,26 +248,41 @@ def write_raw_data(df_dict, use_all_data=False, debug=False, sub_path='/raw_comp
         If df_dict is empty
     '''
 
-    #if len(df_dict.keys()) == 0 or all([type(x)==type(pd.DataFrame()) for x in df_dict.values()]):
     if len(df_dict.keys()) == 0:
-        raise ValueError('df_dict must contain at least item and all must be pandas dataframes')
+        raise ValueError('df_dict must contain at least one item and all must be pandas dataframes')
 
     if use_all_data==True:
         store_path = os.environ['GP_HIST_PATH'] + sub_path
     else:
         store_path = os.environ['my_home_path'] + '/data/space-track-gp-hist-sample' + sub_path
+    paths = [store_path + '/' + data_type + '.pkl' for data_type in df_dict.keys()]
 
-    for data_type, df in df_dict.items():
-        file_name = data_type  + '.pkl.gz'
-        path = store_path + '/' + file_name
-        if debug:
-            print(f'Writing raw data for {data_type} to: {path}')
-        df.to_pickle(path)
     if debug:
-        print(f'Done writing output.')
+        print(f'Saving files to path: {store_path}')
+
+    if threaded:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+            tasks = [executor.submit(__write_task, df, paths[i], debug) for i, df in enumerate(df_dict.values())]
+            for t in concurrent.futures.as_completed(tasks):
+                print(f'Finished saving {t.result()}')
+
+    elif multiproc:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=3) as executor:
+            # tasks = [executor.submit(__write_task, df, paths[i], debug) for i, df in enumerate(df_dict.values())]
+            # for t in concurrent.futures.as_completed(tasks):
+            #     print(f'Finished saving {t.result()}')
+            executor.map(__write_task, list(df_dict.values()), paths, repeat(debug))
+            #if concurrent.futures.wait()
+    else:
+        for i, df in enumerate(df_dict.values()):
+            path = paths[i]
+            __write_task(df, path, debug)
+        if debug:
+            print(f'Done writing output.')
 
 if __name__ == '__main__':
     import argparse
+    from time import time
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--train', action='store_const', const=True, default=False, help='Load train data')
@@ -280,7 +295,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    # Get the NORAD list
+    # Load the NORAD list
     data_types = []
     if args.train:
         data_types.append('train')
@@ -288,20 +303,25 @@ if __name__ == '__main__':
         data_types.append('validate')
     if args.secret:
         data_types.append('secret_test')
-    norad_lists = load_norads(data_types, True)
+    norad_lists = load_norads(data_types, debug=True)
 
-    # Get the pandas dataframe
+    # Load the pandas dataframe
+    ts = time()
     if args.multiprocess:
-        df_dict = load_data_multi(norad_lists, args.use_all_data, True)
+        df_dict = load_data(norad_lists, use_all_data=args.use_all_data, debug=True, multiproc=True)
     elif args.multithreaded:
-        df_dict = load_data_multi(norad_lists, args.use_all_data, True, True)
+        df_dict = load_data(norad_lists, use_all_data=args.use_all_data, debug=True, threaded=True)
     else:
-        df_dict = load_data(norad_lists, args.use_all_data, True)
-
-    for k,v in df_dict.items():
-        print(f'{k} has {len(v)} items:')
-        print(v.head())
+        df_dict = load_data(norad_lists, use_all_data=args.use_all_data, debug=True)
+    print(f'Loading and assembling took {round(time()-ts)} seconds')
 
     # Write output
     if args.write:
-        write_raw_data(df_dict, args.use_all_data, True)
+        ts = time()
+        if args.multiprocess:
+            write_data(df_dict, use_all_data=args.use_all_data, debug=True, multiproc=True)
+        elif args.multithreaded:
+            write_data(df_dict, use_all_data=args.use_all_data, debug=True, threaded=True)
+        else:
+            write_data(df_dict, use_all_data=args.use_all_data, debug=True)
+        print(f'Saving took {round(time()-ts)} seconds')
